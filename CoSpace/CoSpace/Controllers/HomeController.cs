fc73fd1ee27d89b7adce928cf2c506b0a9d3d5c8
Gerardo Lanza <glanza007@gmail.com>
2023-07-11 16:02:35 -0600

    Full Calendar

--- CoSpace/CoSpace/Controllers/HomeController.cs
+++ CoSpace/CoSpace/Controllers/HomeController.cs
@@ -1,5 +1,10 @@
-﻿using CoSpace.Models;
+﻿using CoSpace.Data;
+using CoSpace.Data.Entities;
+using CoSpace.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using System.Diagnostics;
 
 namespace CoSpace.Controllers
@@ -7,10 +12,12 @@ namespace CoSpace.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly DataContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, DataContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
@@ -20,6 +27,39 @@ namespace CoSpace.Controllers
 
         public IActionResult Privacy()
         {
+            return View();
+        }
+
+        public IActionResult Calendar()
+        {
+            //ViewData["events"] = new[]
+            //{
+            //    new Event { Id = 1, Title = "Video for Marisa", StartDate = "2020-11-14"},
+            //    new Event { Id = 2, Title = "Preparation", StartDate = "2020-11-12"},
+            //};
+
+            // Obtener las reservas de la tabla "reservas" desde tu base de datos
+            List<Booking> reservas = _context.Bookings.Include(r => r.Space).ToList();
+            // Convertir las reservas en un formato adecuado para FullCalendar
+            List<object> eventos = new List<object>();
+
+            foreach (Booking reserva in reservas)
+            {
+                // Crear un objeto de evento con los campos requeridos por FullCalendar
+                var evento = new
+                {
+                    id = reserva.Id,
+                    title = reserva.BookingState.ToString(),
+                    start = reserva.StartDate.ToString("yyyy-MM-ddTHH:mm:ss"),
+                    end = reserva.EndDate.ToString("yyyy-MM-ddTHH:mm:ss"),
+                    sala = reserva.Space!.Name
+                };
+
+                eventos.Add(evento);
+            }
+            // Pasar los eventos a la vista "Calendar"
+            ViewBag.Eventos = JsonConvert.SerializeObject(eventos);
+
             return View();
         }
 
